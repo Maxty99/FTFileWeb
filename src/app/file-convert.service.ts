d1f4caba75838b746a994499fcbf93da04728e21
Xavier's Coding Club <maxty99@gmail.com>
2021-05-13 22:34:45 -0400

    Implement Converting component

--- src/app/file-convert.service.ts
+++ src/app/file-convert.service.ts
@@ -1,30 +1,38 @@
 import { Injectable } from '@angular/core';
-import { FTFile, SVG } from 'ftfiles';
+import { FTProcessor, FTBuffer } from 'ftfiles';
 import { saveAs } from 'file-saver';
+
 @Injectable({
   providedIn: 'root',
 })
 export class FileConvertService {
   pathSvgString: string = '';
-  processedFile: FTFile | undefined = undefined;
+  processedFile: FTProcessor | undefined = undefined;
   newFileName: string = '';
   constructor() {}
 
   configure(fileBase64String: string, newFileName: string) {
-    this.pathSvgString = atob(fileBase64String);
+    this.pathSvgString = atob(fileBase64String.split(',')[1]);
     this.newFileName = newFileName;
   }
-  processFile(points: number) {
-    const svgPath = new SVG();
-    svgPath.loadFromString(this.pathSvgString);
-    const ftFile = new FTFile(svgPath);
-    ftFile.processPoints(points);
-    ftFile.discreteTransform();
+  isConfigured(): boolean {
+    return this.newFileName != '' && this.pathSvgString != '';
   }
+  processFile(points: number) {
+    const parser = new DOMParser();
+    const document = parser.parseFromString(
+      this.pathSvgString,
+      'image/svg+xml'
+    );
+    const path = document.querySelector('path');
 
-  saveFile() {
-    if (this.processedFile != undefined) {
-      const fileBuffer = this.processedFile.writeToBuffer();
+    if (path) {
+      const ftProc = new FTProcessor(path);
+      ftProc.processPoints(points);
+      ftProc.discreteTransform();
+      const ftBuf = new FTBuffer();
+      ftBuf.coefficients = ftProc.getCoefficients();
+      const fileBuffer = ftBuf.writeToBuffer();
       const blob = new Blob([fileBuffer], { type: 'application/octet-stream' });
       saveAs(blob, this.newFileName);
     }
--- src/app/file-form/file-form.component.ts
+++ src/app/file-form/file-form.component.ts
@@ -9,40 +9,52 @@ import { FileConvertService } from '../file-convert.service';
   styleUrls: ['./file-form.component.css'],
 })
 export class FileFormComponent implements OnInit {
-  nm: FormControl = new FormControl('', Validators.required);
+  nm: FormControl = new FormControl('');
+  points: FormControl = new FormControl(100);
+  status: string = '';
   filename: string | null = '';
   base64File: string | null = '';
 
   constructor(private fileConvert: FileConvertService) {}
 
-  onChangeForm(event: any): void {
+  onChangeForm(event: any) {
     try {
       const file = event.target.files[0];
-      console.log(typeof file);
       const fReader = new FileReader();
       fReader.readAsDataURL(file);
       fReader.onloadend = (_event: any) => {
-        this.filename = file.name;
+        this.filename = file.name.split('.')[0] + '.ftf';
         this.base64File = _event.target.result;
-        console.log(this.base64File);
+
+        if (!this.base64File?.startsWith('data:image/svg+xml')) {
+          this.status += 'Not svg file\n';
+        } else {
+          this.fileConvert.configure(this.base64File, this.filename);
+          this.status +=
+            'Got (base64 encoded): ' + this.base64File.split(';')[0] + '\n';
+        }
       };
     } catch (error) {
+      this.status += 'no file was selected...';
+      this.status += error;
       this.reset();
-      console.log('no file was selected...');
-      console.log(error);
-      return;
     }
-
-    if (!this.base64File?.startsWith('data:image/svg+xml')) {
-      this.reset();
-      console.log('Not svg file'); // Make ui
-    } else {
-    }
+    this.reset();
   }
 
+  convert() {
+    if (this.fileConvert.isConfigured()) {
+      this.status += 'Converting with ' + this.points.value + ' points... \n';
+      this.fileConvert.processFile(this.points.value);
+      this.status += 'Returning file: ' + this.filename + '\n';
+    } else {
+      this.status += 'no file was selected... \n';
+    }
+  }
   reset() {
     this.filename = null;
     this.base64File = null;
+    this.status = '';
   }
 
   ngOnInit(): void {}
